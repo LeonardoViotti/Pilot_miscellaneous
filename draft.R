a16f2c0d38a816b3a4c84fa82bc65dd179eddcdb
LeonardoViotti <leonardoviotti@gmail.com>
2018-09-12 07:16:11 -0300

    Added pseudocode draft

---
+++ draft.R
@@ -0,0 +1,37 @@
+  #------------------------------------------------------------------------------#
+  
+  #	  LVTP - Corridor buffer
+  
+  #------------------------------------------------------------------------------#
+
+
+  # Porpuse: Create a X km buffer around corridor and calculate descriptive stats
+  # with Establishment census and land cadastre data.
+
+
+  #------------------------------------------------------------------------------#
+  #### LOAD AND PROCESS DATA ####
+
+  #### BUFFER ####
+  
+  #### VILLAGE BUFFER ####
+  
+  #### CADASTRE BUFFER ####
+  
+  #### MATCH EC DATA ####
+  
+  #### EC STATS ####
+    
+  # Jobs
+  
+  # Business
+  
+  #### HEAT MAP ####
+  
+  # Rwanda
+  
+  # Around corriodor
+  
+  #### CADASTRE MAP EXAMPLE ####
+  
+  
