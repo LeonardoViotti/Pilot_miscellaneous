b0ce6252ab0e75e47969302cddd33ddfd0bd1550
LeonardoViotti <leonardoviotti@gmail.com>
2018-09-28 18:41:31 -0400

    fixed regressions

--- feeder_analysis.R
+++ feeder_analysis.R
@@ -10,6 +10,7 @@ library(stargazer)
 
 EXPORT_shapes <- F
 
+RUN_triple_diff <- F
 
 #------------------------------------------------------------------------------#
 #### LOAD AND PROCESS SHAPEFILES ####
@@ -167,16 +168,29 @@ reg_busi3 <- felm(log(N_busi) ~ treated + Shape_AreaKM + Population  | 0 | 0 | C
 #### EXPORT REGRESSIONS ####
 
 
-regbusi.tex <- stargazer(reg_busi1, 
-                         reg_busi2, 
-                         reg_busi3, 
-                         title = "Number of Businesses - Simple Difference",
-                         covariate.labels = c("Connected (1km buffer intersection)", 
-                                              "Area (sq. Km)", 
-                                              "Population",
-                                              "Constant"),
-                         dep.var.labels   = c("Businesses (log)"))
-
+regbusi.tex <-
+  stargazer(reg_busi1, 
+            reg_busi2, 
+            reg_busi3, 
+            title = "Number of Businesses - Simple Difference",
+            covariate.labels = c("Connected (1km buffer intersection)", 
+                                 "Area (sq. Km)", 
+                                 "Population",
+                                 "Constant"),
+            dep.var.labels   = c("Businesses (log)") #,
+            # add.lines = list(
+            #   c("Mean of dependent variable", 
+            #     round(mean(regVill_df$N_busi[complete.cases(regVill_df[,c("N_busi")])], na.rm = T),2),
+            #     round(mean(regVill_df$N_busi[complete.cases(regVill_df[,c("N_busi", "Shape_AreaKM")])], na.rm = T),2),
+            #     round(mean(regVill_df$N_busi[complete.cases(regVill_df[,c("N_busi", "Shape_AreaKM", "Population")])], na.rm = T),2)
+            #     ),
+            #   c("Std. deviation of dependent variable", 
+            #     round(sd(regVill_df$N_busi[complete.cases(regVill_df[,c("N_busi")])], na.rm = T),2),
+            #     round(sd(regVill_df$N_busi[complete.cases(regVill_df[,c("N_busi", "Shape_AreaKM")])], na.rm = T),2),
+            #     round(sd(regVill_df$N_busi[complete.cases(regVill_df[,c("N_busi", "Shape_AreaKM", "Population")])], na.rm = T),2)
+            #     )
+            # )
+  )
 
 regjobs.tex <- stargazer(reg_jobs1, 
                          reg_jobs2, 
@@ -202,144 +216,147 @@ write(regjobs.tex,
 #------------------------------------------------------------------------------#
 #### TRIPLE DIFFERENCE ####
 
-#### Other buffers ####
-nat.buf5 <- gBuffer(connect.buf, width = 4000)
-nat.buf2 <- gBuffer(connect.buf, width = 1000)
 
-#### Regression Variables ####
+if(RUN_triple_diff){
+  
+  #### Other buffers ####
+  nat.buf5 <- gBuffer(connect.buf, width = 4000)
+  nat.buf2 <- gBuffer(connect.buf, width = 1000)
+  
+  #### Regression Variables ####
+  
+  #### Buffer dummies
+  villages_ct@data$buf5 <- 0
+  villages_ct@data$buf5[gIntersects(villages_ct,
+                                    gUnaryUnion(nat.buf5),
+                                    byid = T) %>% as.logical] <- 1
+  
+  villages_ct@data$buf2 <- 0
+  villages_ct@data$buf2[gIntersects(villages_ct,
+                                    gUnaryUnion(nat.buf2),
+                                    byid = T) %>% as.logical] <- 1
+  
+  villages_ct@data$buf1fs <- 0
+  villages_ct@data$buf1fs[gIntersects(villages_ct,
+                                    gUnaryUnion(fs.buf),
+                                    byid = T) %>% as.logical] <- 1
+  
+  
+  villages_ct@data$conected <- 0
+  villages_ct@data$conected[gIntersects(villages_ct,
+                                        gUnaryUnion(connect.buf),
+                                        byid = T) %>% as.logical] <- 1
+  
+  villages_ct@data$isolated <- 0
+  villages_ct@data$isolated[gIntersects(villages_ct,
+                                        gUnaryUnion(isolated.buf),
+                                        byid = T) %>% as.logical] <- 1
+  
+  #### Subset datasets ####
+  analysis_vills_bol_2km <- 
+    !(villages_ct@data$buf2==0 & 
+        villages_ct@data$buf1fs  == 0)
+  
+  analysis_vills_bol_5km <- 
+    !(villages_ct@data$buf5 ==0 & villages_ct@data$buf1fs  == 0)
+  
+  
+  villages_ct2 <- subset(villages_ct, analysis_vills_bol_2km)
+  villages_ct5 <- subset(villages_ct, analysis_vills_bol_5km)
+  
+  
+  #### Regressions ####
+  
+  # Business 2 km
+  reg2km_busi_1 <- felm(log(N_busi) ~ buf2 | 0 | 0 | Code_vill_,
+                        data = villages_ct2 )
+  reg2km_busi_2 <- felm(log(N_busi) ~ buf1fs  | 0 | 0 | Code_vill_,
+                        data = villages_ct2 )
+  reg2km_busi_3 <- felm(log(N_busi) ~ buf2 + buf1fs +  buf2*buf1fs -1 | 0 | 0 | Code_vill_,
+                        data = villages_ct2 )
+  
+  # Jobs 2 km
+  reg2km_jobs_1 <- felm(log(N_jobs) ~ buf2 | 0 | 0 | Code_vill_,
+                        data = villages_ct2 )
+  reg2km_jobs_2 <- felm(log(N_jobs) ~ buf1fs  | 0 | 0 | Code_vill_,
+                        data = villages_ct2 )
+  reg2km_jobs_3 <- felm(log(N_jobs) ~ buf2 + buf1fs +  buf2*buf1fs -1 | 0 | 0 | Code_vill_,
+                        data = villages_ct2 )
+  
+  foo <- lm(log(N_jobs) ~ buf2 + buf1fs +  buf2*buf1fs, 
+            data = villages_ct2)
+  
+  # Business 5 km
+  reg5km_busi_1 <- felm(log(N_busi) ~ buf5 | 0 | 0 | Code_vill_,
+                        data = villages_ct5 )
+  reg5km_busi_2 <- felm(log(N_busi) ~ buf1fs  | 0 | 0 | Code_vill_,
+                        data = villages_ct5 )
+  reg5km_busi_3 <- felm(log(N_busi) ~ buf5 + buf1fs +  buf5*buf1fs -1 | 0 | 0 | Code_vill_,
+                        data = villages_ct5 )
+  
+  # Jobs 5 km
+  reg5km_jobs_1 <- felm(log(N_jobs) ~ buf5 | 0 | 0 | Code_vill_,
+                        data = villages_ct5 )
+  reg5km_jobs_2 <- felm(log(N_jobs) ~ buf1fs  | 0 | 0 | Code_vill_,
+                        data = villages_ct5 )
+  reg5km_jobs_3 <- felm(log(N_jobs) ~ buf5 + buf1fs +  buf5*buf1fs -1 | 0 | 0 | Code_vill_,
+                        data = villages_ct5 )
+  
+  #### Export regressions ####
+  reg2km_busi_tex <- 
+    stargazer(reg2km_busi_1,
+              reg2km_busi_2,
+              reg2km_busi_3,
+              title = "Number of Businesses - Triple Difference: 2km Buffer",
+              covariate.labels = c("Within 2km Intersection", 
+                                   "Within 1km Feeder Road", 
+                                   "Within 2km Intersection X Within 1km Feeder Road"),
+              dep.var.labels   = c("Businesses (log)"),
+              omit.stat = c("f", "ser")
+              )
+  write(reg2km_busi_tex,
+        file = file.path(OUT_tables, "tripleDiff_2km_busi.tex"))
+  
+  reg2km_jobs_tex <- 
+    stargazer(reg2km_jobs_1,
+              reg2km_jobs_2,
+              reg2km_jobs_3,
+              title = "Number of Jobs - Triple Difference: 2km Buffer",
+              covariate.labels = c("Within 2km Intersection", 
+                                   "Within 1km Feeder Road", 
+                                   "Within 2km Intersection X Within 1km Feeder Road"),
+              dep.var.labels   = c("Jobs (log)"),
+              omit.stat = c("f", "ser"))
+  write(reg2km_jobs_tex,
+        file = file.path(OUT_tables, "tripleDiff_2km_jobs.tex"))
+  
+  reg5km_busi_tex <- 
+    stargazer(reg5km_busi_1,
+              reg5km_busi_2,
+              reg5km_busi_3,
+              title = "Number of Businesses - Triple Difference: 5km Buffer",
+              covariate.labels = c("Within 5km Intersection", 
+                                   "Within 1km Feeder Road", 
+                                   "Within 5km Intersection X Within 1km Feeder Road"),
+              dep.var.labels   = c("Businesses (log)"),
+              omit.stat = c("f", "ser"))
+  write(reg5km_busi_tex,
+        file = file.path(OUT_tables, "tripleDiff_5km_busi.tex"))
+  
+  reg5km_jobs_tex <- 
+    stargazer(reg5km_jobs_1,
+              reg5km_jobs_2,
+              reg5km_jobs_3,
+              title = "Number of Jobs - Triple Difference: 5km Buffer",
+              covariate.labels = c("Within 5km Intersection", 
+                                   "Within 1km Feeder Road", 
+                                   "Within 5km Intersection X Within 1km Feeder Road"),
+              dep.var.labels   = c("Jobs (log)"),
+              omit.stat = c("f", "ser"))
+  write(reg5km_jobs_tex,
+        file = file.path(OUT_tables, "tripleDiff_5km_jobs.tex"))
 
-#### Buffer dummies
-villages_ct@data$buf5 <- 0
-villages_ct@data$buf5[gIntersects(villages_ct,
-                                  gUnaryUnion(nat.buf5),
-                                  byid = T) %>% as.logical] <- 1
-
-villages_ct@data$buf2 <- 0
-villages_ct@data$buf2[gIntersects(villages_ct,
-                                  gUnaryUnion(nat.buf2),
-                                  byid = T) %>% as.logical] <- 1
-
-villages_ct@data$buf1fs <- 0
-villages_ct@data$buf1fs[gIntersects(villages_ct,
-                                  gUnaryUnion(fs.buf),
-                                  byid = T) %>% as.logical] <- 1
-
-
-villages_ct@data$conected <- 0
-villages_ct@data$conected[gIntersects(villages_ct,
-                                      gUnaryUnion(connect.buf),
-                                      byid = T) %>% as.logical] <- 1
-
-villages_ct@data$isolated <- 0
-villages_ct@data$isolated[gIntersects(villages_ct,
-                                      gUnaryUnion(isolated.buf),
-                                      byid = T) %>% as.logical] <- 1
-
-#### Subset datasets ####
-analysis_vills_bol_2km <- 
-  !(villages_ct@data$buf2==0 & 
-      villages_ct@data$buf1fs  == 0)
-
-analysis_vills_bol_5km <- 
-  !(villages_ct@data$buf5 ==0 & villages_ct@data$buf1fs  == 0)
-
-
-villages_ct2 <- subset(villages_ct, analysis_vills_bol_2km)
-villages_ct5 <- subset(villages_ct, analysis_vills_bol_5km)
-
-
-#### Regressions ####
-
-# Business 2 km
-reg2km_busi_1 <- felm(log(N_busi) ~ buf2 | 0 | 0 | Code_vill_,
-                      data = villages_ct2 )
-reg2km_busi_2 <- felm(log(N_busi) ~ buf1fs  | 0 | 0 | Code_vill_,
-                      data = villages_ct2 )
-reg2km_busi_3 <- felm(log(N_busi) ~ buf2 + buf1fs +  buf2*buf1fs -1 | 0 | 0 | Code_vill_,
-                      data = villages_ct2 )
-
-# Jobs 2 km
-reg2km_jobs_1 <- felm(log(N_jobs) ~ buf2 | 0 | 0 | Code_vill_,
-                      data = villages_ct2 )
-reg2km_jobs_2 <- felm(log(N_jobs) ~ buf1fs  | 0 | 0 | Code_vill_,
-                      data = villages_ct2 )
-reg2km_jobs_3 <- felm(log(N_jobs) ~ buf2 + buf1fs +  buf2*buf1fs -1 | 0 | 0 | Code_vill_,
-                      data = villages_ct2 )
-
-foo <- lm(log(N_jobs) ~ buf2 + buf1fs +  buf2*buf1fs, 
-          data = villages_ct2)
-
-# Business 5 km
-reg5km_busi_1 <- felm(log(N_busi) ~ buf5 | 0 | 0 | Code_vill_,
-                      data = villages_ct5 )
-reg5km_busi_2 <- felm(log(N_busi) ~ buf1fs  | 0 | 0 | Code_vill_,
-                      data = villages_ct5 )
-reg5km_busi_3 <- felm(log(N_busi) ~ buf5 + buf1fs +  buf5*buf1fs -1 | 0 | 0 | Code_vill_,
-                      data = villages_ct5 )
-
-# Jobs 5 km
-reg5km_jobs_1 <- felm(log(N_jobs) ~ buf5 | 0 | 0 | Code_vill_,
-                      data = villages_ct5 )
-reg5km_jobs_2 <- felm(log(N_jobs) ~ buf1fs  | 0 | 0 | Code_vill_,
-                      data = villages_ct5 )
-reg5km_jobs_3 <- felm(log(N_jobs) ~ buf5 + buf1fs +  buf5*buf1fs -1 | 0 | 0 | Code_vill_,
-                      data = villages_ct5 )
-
-#### Export regressions ####
-reg2km_busi_tex <- 
-  stargazer(reg2km_busi_1,
-            reg2km_busi_2,
-            reg2km_busi_3,
-            title = "Number of Businesses - Triple Difference: 2km Buffer",
-            covariate.labels = c("Within 2km Intersection", 
-                                 "Within 1km Feeder Road", 
-                                 "Within 2km Intersection X Within 1km Feeder Road"),
-            dep.var.labels   = c("Businesses (log)"),
-            omit.stat = c("f", "ser")
-            )
-write(reg2km_busi_tex,
-      file = file.path(OUT_tables, "tripleDiff_2km_busi.tex"))
-
-reg2km_jobs_tex <- 
-  stargazer(reg2km_jobs_1,
-            reg2km_jobs_2,
-            reg2km_jobs_3,
-            title = "Number of Jobs - Triple Difference: 2km Buffer",
-            covariate.labels = c("Within 2km Intersection", 
-                                 "Within 1km Feeder Road", 
-                                 "Within 2km Intersection X Within 1km Feeder Road"),
-            dep.var.labels   = c("Jobs (log)"),
-            omit.stat = c("f", "ser"))
-write(reg2km_jobs_tex,
-      file = file.path(OUT_tables, "tripleDiff_2km_jobs.tex"))
-
-reg5km_busi_tex <- 
-  stargazer(reg5km_busi_1,
-            reg5km_busi_2,
-            reg5km_busi_3,
-            title = "Number of Businesses - Triple Difference: 5km Buffer",
-            covariate.labels = c("Within 5km Intersection", 
-                                 "Within 1km Feeder Road", 
-                                 "Within 5km Intersection X Within 1km Feeder Road"),
-            dep.var.labels   = c("Businesses (log)"),
-            omit.stat = c("f", "ser"))
-write(reg5km_busi_tex,
-      file = file.path(OUT_tables, "tripleDiff_5km_busi.tex"))
-
-reg5km_jobs_tex <- 
-  stargazer(reg5km_jobs_1,
-            reg5km_jobs_2,
-            reg5km_jobs_3,
-            title = "Number of Jobs - Triple Difference: 5km Buffer",
-            covariate.labels = c("Within 5km Intersection", 
-                                 "Within 1km Feeder Road", 
-                                 "Within 5km Intersection X Within 1km Feeder Road"),
-            dep.var.labels   = c("Jobs (log)"),
-            omit.stat = c("f", "ser"))
-write(reg5km_jobs_tex,
-      file = file.path(OUT_tables, "tripleDiff_5km_jobs.tex"))
-
-
+}
 
 #------------------------------------------------------------------------------#
 #### EXPORT SHAPEFILES ####
@@ -383,108 +400,114 @@ if (EXPORT_shapes){
 #### AUXILIARY MAPS ####
 
 
-# Buffers for plot
-fs4.buf <- gBuffer(fs_simp[4,], width = 1000, byid = T)
-nat15<- nat_roads[nat_roads$ROAD_NO == "RN 29",]
-nat15.buf <- gBuffer(nat_roads[nat_roads$ROAD_NO == "RN 29",], width = 1000)
-
-connect4.buf <- intersect(fs4.buf, nat.buf)
-
-nat4.buf2 <- gBuffer(connect4.buf, width = 1000)
+if(RUN_aux_maps){
 
 
+  # Buffers for plot
+  fs4.buf <- gBuffer(fs_simp[4,], width = 1000, byid = T)
+  nat15<- nat_roads[nat_roads$ROAD_NO == "RN 29",]
+  nat15.buf <- gBuffer(nat_roads[nat_roads$ROAD_NO == "RN 29",], width = 1000)
+  
+  connect4.buf <- intersect(fs4.buf, nat.buf)
+  
+  nat4.buf2 <- gBuffer(connect4.buf, width = 1000)
+  
+  
+  
+  
+  
+  
+  
+  png(file.path(OUT_maps, "indentification.png"),
+      units="in",
+      res=300,
+      width=5, 
+      height=5)
+    
+    par(mfrow=c(2,2),
+        cex.main = .7)
+  
+  
+    # 1km buffer
+    plot(fs.buf[4,], 
+         col = rgb(red = 0, green = 0.7, blue = 0, alpha = 0.3), 
+         border = NA)
+    title("1km feeder buffer")
+    
+    plot(fs_simp[4,], col = "forestgreen", add = T)
+    plot(nat15, col = "navyblue", add = T)
+    
+    
+    
+    # intersection buffer intersection
+    plot(fs.buf[4,], 
+         col = rgb(red = 0, green = 0.7, blue = 0, alpha = 0.3), 
+         border = NA)
+    
+    title("Connected (1km buffer intersection)")
+    
+    plot(fs_simp[4,], col = "forestgreen", add = T)
+    
+    plot(nat15.buf, 
+         border = "blue",
+         add = T)
+    
+    plot(connect4.buf, 
+         col = rgb(red = 1, green = 0, blue = 0, alpha = 0.3), 
+         border = "red",
+         add = T)
+    
+    plot(nat15, col = "navyblue", add = T)
+    
+    
+    
+    
+    # plot 2
+    plot(nat4.buf2,
+         col = rgb(red = 0, green = 0, blue = 1, alpha = 0.3), 
+         border = NA)
+    title("2km intersecetion buffer")
+    plot(fs.buf[4,],        
+         col = rgb(red = 0, green = 0.7, blue = 0, alpha = 0.3), 
+         border = NA, add = T)
+    plot(connect4.buf, 
+         col = rgb(red = 1, green = 0, blue = 0, alpha = 0.3), 
+         border = NA,
+         add = T)
+    plot(fs_simp[4,], 
+         col = "forestgreen",
+         add = T)
+    plot(nat15, col = "navyblue", add = T)
+    
+    
+    
+    # Legend
+    plot(NULL ,xaxt='n',yaxt='n',bty='n',ylab='',xlab='', xlim=0:1, ylim=0:1)
+    legend("center",
+           #ncol=2,
+           legend = c("1m buffer around feeder road",
+                      "Connected (1km buffer intersection)",
+                      "2km buffer around intersection", 
+                      "1km buffer around national road", 
+                      "Feeder Road", 
+                      "National road"), 
+           lty= c(0,0,0,0,1,1),
+           col = c("forestgreen", 
+                   "red", 
+                   "blue", 
+                   "blue", 
+                   "forestgreen", 
+                   "navyblue"),
+           fill= c( rgb(red = 0, green = 0.7, blue = 0, alpha = 0.3), 
+                    rgb(red = 1, green = 0, blue = 0, alpha = 0.3), 
+                    rgb(red = 0, green = 0, blue = 1, alpha = 0.3), 
+                    NA, 
+                    NA, 
+                    NA),
+           border = c(NA,NA,NA,"blue",NA,NA),
+           cex = .5)
+  
+  dev.off()
+  
 
-
-
-
-
-png(file.path(OUT_maps, "indentification.png"),
-    units="in",
-    res=300,
-    width=5, 
-    height=5)
-  
-  par(mfrow=c(2,2),
-      cex.main = .7)
-
-
-  # 1km buffer
-  plot(fs.buf[4,], 
-       col = rgb(red = 0, green = 0.7, blue = 0, alpha = 0.3), 
-       border = NA)
-  title("1km feeder buffer")
-  
-  plot(fs_simp[4,], col = "forestgreen", add = T)
-  plot(nat15, col = "navyblue", add = T)
-  
-  
-  
-  # intersection buffer intersection
-  plot(fs.buf[4,], 
-       col = rgb(red = 0, green = 0.7, blue = 0, alpha = 0.3), 
-       border = NA)
-  
-  title("Connected (1km buffer intersection)")
-  
-  plot(fs_simp[4,], col = "forestgreen", add = T)
-  
-  plot(nat15.buf, 
-       border = "blue",
-       add = T)
-  
-  plot(connect4.buf, 
-       col = rgb(red = 1, green = 0, blue = 0, alpha = 0.3), 
-       border = "red",
-       add = T)
-  
-  plot(nat15, col = "navyblue", add = T)
-  
-  
-  
-  
-  # plot 2
-  plot(nat4.buf2,
-       col = rgb(red = 0, green = 0, blue = 1, alpha = 0.3), 
-       border = NA)
-  title("2km intersecetion buffer")
-  plot(fs.buf[4,],        
-       col = rgb(red = 0, green = 0.7, blue = 0, alpha = 0.3), 
-       border = NA, add = T)
-  plot(connect4.buf, 
-       col = rgb(red = 1, green = 0, blue = 0, alpha = 0.3), 
-       border = NA,
-       add = T)
-  plot(fs_simp[4,], 
-       col = "forestgreen",
-       add = T)
-  plot(nat15, col = "navyblue", add = T)
-  
-  
-  
-  # Legend
-  plot(NULL ,xaxt='n',yaxt='n',bty='n',ylab='',xlab='', xlim=0:1, ylim=0:1)
-  legend("center",
-         #ncol=2,
-         legend = c("1m buffer around feeder road",
-                    "Connected (1km buffer intersection)",
-                    "2km buffer around intersection", 
-                    "1km buffer around national road", 
-                    "Feeder Road", 
-                    "National road"), 
-         lty= c(0,0,0,0,1,1),
-         col = c("forestgreen", 
-                 "red", 
-                 "blue", 
-                 "blue", 
-                 "forestgreen", 
-                 "navyblue"),
-         fill= c( rgb(red = 0, green = 0.7, blue = 0, alpha = 0.3), 
-                  rgb(red = 1, green = 0, blue = 0, alpha = 0.3), 
-                  rgb(red = 0, green = 0, blue = 1, alpha = 0.3), 
-                  NA, 
-                  NA, 
-                  NA),
-         border = c(NA,NA,NA,"blue",NA,NA),
-         cex = .5)
-
-dev.off()
+}
